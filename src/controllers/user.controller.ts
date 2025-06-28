f60d61c36f0e5a71b210b6fde0d9afe7af26e5c8
shaikmahenoor024 <shaikmahenoor024@gmail.com>
2025-06-28 12:55:16 +0530

    updated dashboard

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express';
 import {User} from '../models/user.model';
+import { extractIdFromToken } from '../utils/jwt';
 
 export const getProfile = async (req: Request, res: Response) => {
-  const user = await User.findById((req as any).user.id).select('-password');
+  const userId = extractIdFromToken(req.headers.authorization?.split(' ')[1] || '');
+  if (!userId) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  const user= await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  } 
   res.json(user);
 };
