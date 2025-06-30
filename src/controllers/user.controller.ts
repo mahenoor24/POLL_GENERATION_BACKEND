cbe8ce81d06d84a9cc7a0a603fa366dc25ceb1d6
shaikmahenoor024 <shaikmahenoor024@gmail.com>
2025-06-30 12:42:17 +0530

    updated backend

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,16 +2,32 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import { extractIdFromToken } from "../utils/jwt";
 
+class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
 export const getProfile = async (req: Request, res: Response) => {
-  const jwtUser = extractIdFromToken(
-    req.headers.authorization?.split(" ")[1] || ""
-  );
-  if (!jwtUser) {
-    return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const jwtUser = extractIdFromToken(
+      req.headers.authorization?.split(" ")[1] || ""
+    );
+    if (!jwtUser) {
+      throw new ValidationError("Unauthorized");
+    }
+    const user = await User.findById(jwtUser.id);
+    if (!user) {
+      throw new ValidationError("User not found");
+    }
+    res.json(user);
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ message: error.message });
+    } else {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   }
-  const user = await User.findById(jwtUser.id);
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  res.json(user);
 };
